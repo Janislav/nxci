e0baf6953fe7720f78765b81aa336de24d97ed7a
Janislav <jan.boerner@nexiles.com>
2015-02-10 10:38:51 +0100

    implemented give me 5 and tests for it

--- nxci/tasks.py
+++ nxci/tasks.py
@@ -1,2 +1,5 @@
 def give_me_three():
     return 3
+
+def give_me_five():
+    return 5
--- tests.py
+++ tests.py
@@ -1,10 +1,14 @@
 import unittest
-from nxci.tasks import give_me_three
+from nxci.tasks import give_me_three, give_me_five
 
 class NxCi(unittest.TestCase):
 
     def test_give_me_three(self):
         self.assertEqual(give_me_three(), 3)
+
+
+    def test_give_me_five(self):
+        self.assertEqual(give_me_five(),5)
 
 if __name__ == '__main__':
     unittest.main()
